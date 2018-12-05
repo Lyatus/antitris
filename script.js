075b4812959dd72bebe00e3062edd183aa33c816
Lucien Catonnet <lutopia@hotmail.fr>
2018-12-05 22:40:38 +0100

    Playable version without score for now

--- script.js
+++ script.js
@@ -6,19 +6,22 @@ var grid = {};
 var gridElement = document.getElementById('grid');
 var state;
 var piece = {};
-var canvas;
-var gl = null;
+var tick = 0;
+var redraw = true;
+var score, level
 
 function keydown(e){
 	console.log(e);
 	switch(e.key){
 		case 'ArrowLeft':
 			piece.x--;
-			if(pieceCollide()) piece.x++;
+			if(piece_collide()) piece.x++;
+			else redraw = true;
 			break;
 		case 'ArrowRight':
 			piece.x++;
-			if(pieceCollide()) piece.x--;
+			if(piece_collide()) piece.x--;
+			else redraw = true;
 			break;
 		case 'ArrowUp':
 			var tmp = piece.c[-2];
@@ -26,66 +29,84 @@ function keydown(e){
 			piece.c[-1] = piece.c[0];
 			piece.c[0] = piece.c[1];
 			piece.c[1] = tmp;
+			redraw = true;
 			break;
 	}
 }
 function update(){
 	switch(state){
 		case 'playing':
-			piece.y++;
-			if(pieceCollide()){
-				piece.y--;
-				if(piece.y==0)
-					state = 'gameover';
-				else{ // Fixate piece
-					for(var i=-2;i<2;i++)
-						cellSet(piece.x+i,piece.y,piece.c[i]);
-					resetPiece();
-					state = 'falling';
+			if(!(tick%20)){
+				piece.y++;
+				if(piece_collide()){
+					piece.y--;
+					if(piece.y==0)
+						state = 'gameover';
+					else{ // Fixate piece
+						for(var i=-2;i<2;i++)
+							cell_set(piece.x+i,piece.y,piece.c[i]);
+						reset_piece();
+						state = 'falling';
+						return update(); // Do a falling update to avoid useless stalling
+					}
 				}
+				redraw = true;
 			}
 			break;
 		case 'falling':
-			var over = true;
-			for(var x=0;x<width;x++){
-				for(var y=height-1;y>0;y--){
-					if(cell(x,y)==VOID && cell(x,y-1)!=VOID){
-						cellSet(x,y,cell(x,y-1));
-						cellSet(x,y-1,VOID);
-						over = false;
+			if(!(tick%10)){
+				var over = true;
+				for(var x=0;x<width;x++){
+					for(var y=height-1;y>0;y--){
+						if(cell(x,y)==VOID && cell(x,y-1)!=VOID){
+							cell_set(x,y,cell(x,y-1));
+							cell_set(x,y-1,VOID);
+							over = false;
+						}
 					}
 				}
+				if(over)
+					state = 'finding';
+				redraw = true;
 			}
-			if(over)
-				state = 'finding';
 			break;
 		case 'finding':
-			var tetromino = findTetromino();
+			var tetromino = find_tetromino();
 			if(tetromino){
 				for(var i=0;i<4;i++)
 					grid[tetromino[i]] = FLASH;
+				showingStartTick = tick;
+				score += 100;
 				state = 'showing';
 			}else{ // No tetromino found, go back to playing
 				state = 'playing';
 			}
+			redraw = true;
 			break;
 		case 'showing':
-			for(var x=0;x<width;x++)
-				for(var y=0;y<height;y++)
-					if(cell(x,y)==FLASH)
-						cellSet(x,y,VOID);
-			state = 'falling';
+			if(tick<showingStartTick+50){
+				for(var x=0;x<width;x++)
+					for(var y=0;y<height;y++)
+						if(cell(x,y)==FLASH)
+							cell_set(x,y,VOID);
+				state = 'falling';
+				update_score();
+				redraw = true;
+			}
 			break;
 		case 'gameover':
 			reset();
 			break;
 	}
-	draw();
+	if(redraw) {
+		draw();
+	}
+	tick++;
 }
 function cell(x,y){
 	return grid[x+'_'+y];
 }
-function cellSet(x,y,v){
+function cell_set(x,y,v){
 	grid[x+'_'+y] = v;
 }
 function draw(){
@@ -94,22 +115,25 @@ function draw(){
 	if(state=='playing')
 		for(var i=-2;i<2;i++)
 			document.getElementById((piece.x+i)+'_'+piece.y).style.backgroundColor = piece.c[i];
+	redraw = false;
 }
 function reset(){
 	state = 'playing';
 	for(var x=0;x<width;x++)
 		for(var y=0;y<height;y++)
-			cellSet(x,y,VOID);
-	resetPiece();
+			cell_set(x,y,VOID);
+	reset_piece();
+	score = 0;
+	update_score();
 }
-function resetPiece(){
+function reset_piece(){
 	piece.x = width/2;
 	piece.y = 0;
 	piece.c = [];
 	for(var i=-2;i<2;i++)
 		piece.c[i] = colors[parseInt(Math.random()*colors.length)];
 }
-function pieceCollide(){
+function piece_collide(){
 	if(piece.y>=height
 	|| piece.x-2<0
 	|| piece.x+1>=width)
@@ -119,27 +143,34 @@ function pieceCollide(){
 			return true;
 	return false;
 }
-function findTetromino(){
+function find_tetromino(){
 	for(var y=0;y<height;y++)
 		for(var x=0;x<width;x++)
 			if(cell(x,y)!=VOID){
 				var tetromino = [];
-				findTetrominoInner(tetromino,x,y,cell(x,y));
+				find_tetromino_inner(tetromino,x,y,cell(x,y));
 				if(tetromino.length==4)
 					return tetromino;
 			}
 	return false;
 }
-function findTetrominoInner(tetromino,x,y,color){
+function find_tetromino_inner(tetromino,x,y,color){
 	var id = x+'_'+y;
 	if(tetromino.length<4 && x>=0 && y>=0 && x<width && y<height
 	&& tetromino.indexOf(id)<0 && cell(x,y)==color){
 		tetromino.push(id);
-		findTetrominoInner(tetromino,x-1,y,color);
-		findTetrominoInner(tetromino,x+1,y,color);
-		findTetrominoInner(tetromino,x,y-1,color);
-		findTetrominoInner(tetromino,x,y+1,color);
+		find_tetromino_inner(tetromino,x-1,y,color);
+		find_tetromino_inner(tetromino,x+1,y,color);
+		find_tetromino_inner(tetromino,x,y-1,color);
+		find_tetromino_inner(tetromino,x,y+1,color);
 	}
+}
+function update_score() {
+	document.getElementById('score').innerHTML = score;
+	document.getElementById('level').innerHTML = level = compute_level();
+}
+function compute_level() {
+	return Math.ceil(Math.log2(Math.max(1,score)));
 }
 
 // Initialize hooks
@@ -155,20 +186,4 @@ for(var y=0;y<height;y++){
 }
 
 reset();
-setInterval(update,100);
-
-// Initialize canvas
-/*
-canvas = document.getElementById('canvas');
-gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-if(!gl) alert('WebGL is unavailable');
-else{ // Start game
-	gl.clearColor(0.0,0.0,0.0,1.0);
-	gl.clearDepth(1.0);
-	gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-	gl.depthFunc(gl.LEQUAL);
-	resize();
-	reset();
-	setInterval(update,100);
-}
-*/
+setInterval(update,10);
